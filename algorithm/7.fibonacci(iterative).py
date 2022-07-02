0c6f056f994ec7ad94941253ef77853e5bba83d1
jmsmg <jmsmg1@me.com>
2022-07-02 13:08:49 +0900

    효율성 개선 메모라이징

--- algorithm/7.fibonacci(iterative).py
+++ algorithm/7.fibonacci(iterative).py
@@ -1,11 +1,21 @@
+# n = int(input())
+
+# a = 1
+# b = 1
+# if n == 1 or n == 2:
+#     print(1)
+# else:
+#     for i in range(1, n):
+#         a, b = b, b + a
+
+# print(a)
+
 n = int(input())
 
-a = 1
-b = 1
-if n == 1 or n == 2:
-    print(1)
-else:
-    for i in range(1, n):
-        a, b = b, b + a
+f = [0] * (n+1)
+if (n > 0):
+    f[1] = 1
+    for i in range(2, n+1):
+        f[i] = f[i - 1] + f[i - 2]
 
-print(a)
+print(f[n])
